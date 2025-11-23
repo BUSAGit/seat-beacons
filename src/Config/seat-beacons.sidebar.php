5c0578094d1eb81748b2ba3476ede88f58db9c88
BUSAGit <93844647+BUSAGit@users.noreply.github.com>
2025-11-23 11:00:36 +0900

    Update seat-beacons.sidebar.php

--- src/Config/seat-beacons.sidebar.php
+++ src/Config/seat-beacons.sidebar.php
@@ -1,10 +1,13 @@
 <?php
-return [
-    [
-        //'name'       => 'seat-beacons::index',
-        'name'      => 'Yump Beacons',
-        'icon'       => 'fas fa-lightbulb',
-        'permission' => 'seat-beacons.access',
-        'route'      => 'seat-beacons::index',
-    ],
+return 
+[
+     'tools' => [
+        [
+            'name'       => 'seat-beacons::index',
+            'label'      => 'Yump Beacons',
+            'icon'       => 'fas fa-lightbulb',
+            'permission' => 'seat-beacons.access',
+            'route'      => 'seat-beacons::index',
+        ],
+     ]
 ];
